0020e6aa370e680bbcbf369cc534da3a0c6eef24
Alexey-Gaidai <1123_666@mail.ru>
2023-03-06 00:03:03 +0700

    dev: получение оборудования, отображние(тест)

---
+++ ReestrClient/FormReestr.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace ReestrClient
+{
+    partial class FormReestr
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(6, 67);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(764, 590);
+            this.treeView1.TabIndex = 0;
+            // 
+            // FormReestr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 663);
+            this.Controls.Add(this.treeView1);
+            this.Name = "FormReestr";
+            this.Text = "FormReestr";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TreeView treeView1;
+    }
+}
---
+++ ReestrClient/Service/DataImpl.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using ReestrClient.Models;
+using ReestrClient.ServiceUseCases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReestrClient.Service
+{
+    internal class DataImpl : DataUseCases
+    {
+        public async Task<(List<Equipment>, string)> GetEquipment(string tokenKey, string parameter)
+        {
+            string data;
+            var baseAddress = new Uri("https://localhost:7073");
+            string url = $"/api/Equipments/";
+            string message = "";
+            try
+            {
+                using (var client = new HttpClient(new HttpClientHandler()) { BaseAddress = baseAddress })
+                {
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {tokenKey}");
+                    var result = await client.GetAsync(url);
+                    var bytes = await result.Content.ReadAsByteArrayAsync();
+                    Encoding encoding = Encoding.GetEncoding("utf-8");
+                    data = encoding.GetString(bytes, 0, bytes.Length);
+                    message = result.StatusCode.ToString();
+                    result.EnsureSuccessStatusCode();
+
+                }
+                var equipments = JsonConvert.DeserializeObject<List<Equipment>>(data);
+                return (equipments, message);
+            }
+            catch
+            {
+                return (default, message);
+            }
+
+        }
+    }
+}
